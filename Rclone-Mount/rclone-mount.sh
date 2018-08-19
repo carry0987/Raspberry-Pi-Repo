79adc79c6ab9c1d9be7c3b69279f8edc2b7a2535
carry0987 <carry0987@users.noreply.github.com>
2018-08-19 16:07:10 +0800

    Add Rclone-Mount tools

---
+++ Rclone-Mount/rclone-mount.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+read -p 'Please enter the path that you want to mount with' mount
+if [ -z $mount ]; then
+    mkdir -p $mount
+    chmod a+rx $mount
+    read -p 'Please enter the name of remote' rclone
+    if [ -z $rclone ]; then
+        rclone mount "$rclone": $mount --allow-other --allow-non-empty --vfs-cache-mode writes &
+    else
+        echo 'The name of remote should not be empty !'
+        exit 0
+    fi
+else
+    echo 'The path should not be empty !'
+    exit 0
+fi
+
+#Make rclone auto mount at boot
+wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Rclone-Mount/rclone.service
+mv -v rclone.service /etc/systemd/system
+chmod 660 /etc/systemd/system/rclone.service
+systemctl daemon-reload
+systemctl start rclone
+systemctl enable rclone
+systemctl status rclone
+echo 'Wait 5 seconds to reboot...'
+sleep 10
+reboot
