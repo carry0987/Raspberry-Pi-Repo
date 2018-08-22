e88ea8fc91883446529fa3b34ac9e1662d01b326
carry0987 <carry0987@users.noreply.github.com>
2018-08-23 01:35:33 +0800

    Add Resource Monitor

--- Tools/tools.sh
+++ Tools/tools.sh
@@ -15,120 +15,128 @@ echo '10) Get Pi Voltage'
 echo '11) Set TCP-BBR'
 echo '12) Update Packages'
 echo '13) Update RPi kernal'
-echo '14) Exit'
+echo '14) Resource Monitor (Sort By CPU)'
+echo '15) Resource Monitor (Sort By Memory)'
+echo '16) Exit'
 read -p 'Which tool do you want to use ? ' tool
 
 #Detect tools
 case $tool in
-  1)
-    read -p 'Please enter your link>' link
-    read -e -p 'Please enter the path that you want to save this file, or just leave blank if you want to save to current path>' download_path
-    if [ -z $download_path ]; then
-        wget --content-disposition $link
-    else
-        wget -P $download_path --content-disposition $link
-    fi
+    1)
+        read -p 'Please enter your link>' link
+        read -e -p 'Please enter the path that you want to save this file, or just leave blank if you want to save to current path>' download_path
+        if [ -z $download_path ]; then
+            wget --content-disposition $link
+        else
+            wget -P $download_path --content-disposition $link
+        fi
     ;;
-  2)
-    read -e -p 'Please enter your path of link list>' link_list
-    read -e -p 'Please enter the path that you want to save this file, or just leave blank if you want to save to current path>' download_list_path
-    if [ -z $download_list_path ]; then
-        wget --content-disposition -i $link_list
-    else
-        wget -P $download_list_path --content-disposition -i $link_list
-    fi
+    2)
+        read -e -p 'Please enter your path of link list>' link_list
+        read -e -p 'Please enter the path that you want to save this file, or just leave blank if you want to save to current path>' download_list_path
+        if [ -z $download_list_path ]; then
+            wget --content-disposition -i $link_list
+        else
+            wget -P $download_list_path --content-disposition -i $link_list
+        fi
     ;;
-  3)
-    read -e -p 'Enter your path>' count_path
-    find $count_path -type f |wc -l
+    3)
+        read -e -p 'Enter your path>' count_path
+        find $count_path -type f |wc -l
     ;;
-  4)
-    read -e -p 'Enter file or folder that you want to remove>' file_or_folder
-    read -p 'Do you really want to remove '$file_or_folder' ? [Y/N]' var
-    if [[ $var =~ ^([Yy])+$ ]]; then
-      rm -vRf $file_or_folder
-    elif [[ $var =~ ^([Nn])+$ ]]; then
-      bash tools.sh
-    else
-      echo 'You can only choose yes or no'
-    fi
+    4)
+        read -e -p 'Enter file or folder that you want to remove>' file_or_folder
+        read -p 'Do you really want to remove '$file_or_folder' ? [Y/N]' var
+        if [[ $var =~ ^([Yy])+$ ]]; then
+          rm -vRf $file_or_folder
+        elif [[ $var =~ ^([Nn])+$ ]]; then
+          bash tools.sh
+        else
+          echo 'You can only choose yes or no'
+        fi
     ;;
-  5)
-    /etc/init.d/cron status
+    5)
+        /etc/init.d/cron status
     ;;
-  6)
-    read -e -p 'Please enter the file or directory which you want to upload>' upload_file
-    read -p 'Please enter the remote path that you want to save>' upload_path
-    prefix_file=${upload_file//\'/\'\"\'\"\'}
-    prefix_path=${upload_path//\'/\'\"\'\"\'}
-    su $USER -c "rclone copy -v --stats 1s '$prefix_file' '$prefix_path'"
+    6)
+        read -e -p 'Please enter the file or directory which you want to upload>' upload_file
+        read -p 'Please enter the remote path that you want to save>' upload_path
+        prefix_file=${upload_file//\'/\'\"\'\"\'}
+        prefix_path=${upload_path//\'/\'\"\'\"\'}
+        su $USER -c "rclone copy -v --stats 1s '$prefix_file' '$prefix_path'"
     ;;
-  7)
-    read -p 'Please enter the name of remote>' rclone_name
-    read -p 'Please enter the file or directory which you want to move from> '$rclone_name':/' move_from
-    read -p 'Please enter the path that you want to move to> '$rclone_name':/' move_to
-    prefix_from=${move_from//\'/\'\"\'\"\'}
-    prefix_to=${move_to//\'/\'\"\'\"\'}
-    move_to_path=`basename $prefix_from`
-    su $USER -c "rclone move -v --stats 1s '$rclone_name:/$prefix_from' '$rclone_name:/$prefix_to/$move_to_path'"
+    7)
+        read -p 'Please enter the name of remote>' rclone_name
+        read -p 'Please enter the file or directory which you want to move from> '$rclone_name':/' move_from
+        read -p 'Please enter the path that you want to move to> '$rclone_name':/' move_to
+        prefix_from=${move_from//\'/\'\"\'\"\'}
+        prefix_to=${move_to//\'/\'\"\'\"\'}
+        move_to_path=`basename $prefix_from`
+        su $USER -c "rclone move -v --stats 1s '$rclone_name:/$prefix_from' '$rclone_name:/$prefix_to/$move_to_path'"
     ;;
-  8)
-    read -p 'Please enter the file or directory which you want to delete>' delete_file_or_dir
-    prefix_delete=${delete_file_or_dir//\'/\'\"\'\"\'}
-    su $USER -c "rclone delete -v --stats 1s '$prefix_delete'"
+    8)
+        read -p 'Please enter the file or directory which you want to delete>' delete_file_or_dir
+        prefix_delete=${delete_file_or_dir//\'/\'\"\'\"\'}
+        su $USER -c "rclone delete -v --stats 1s '$prefix_delete'"
     ;;
-  9)
-    vcgencmd measure_temp
+    9)
+        vcgencmd measure_temp
     ;;
-  10)
-    for id in core sdram_c sdram_i sdram_p ; do \
-      echo -e "$id:\t$(vcgencmd measure_volts $id)" ; \
-    done
+    10)
+        for id in core sdram_c sdram_i sdram_p ; do \
+          echo -e "$id:\t$(vcgencmd measure_volts $id)" ; \
+        done
     ;;
-  11)
-    #Check if TCP-BBR has already setup
-    if [ `grep -c "net.core.default_qdisc=fq" /etc/sysctl.conf` -eq '1' ]; then
-        bbr_qdisc=1
-    else
-        bash -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
-        echo 'Successful setting net core'
-        bbr_qdisc=2
-    fi
+    11)
+        #Check if TCP-BBR has already setup
+        if [ `grep -c "net.core.default_qdisc=fq" /etc/sysctl.conf` -eq '1' ]; then
+            bbr_qdisc=1
+        else
+            bash -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
+            echo 'Successful setting net core'
+            bbr_qdisc=2
+        fi
 
-    if [ `grep -c "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf` -eq '1' ]; then
-        bbr_tcc=1
-    else
-        bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
-        echo 'Successful setting tcp congestion control'
-        bbr_tcc=2
-    fi
+        if [ `grep -c "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf` -eq '1' ]; then
+            bbr_tcc=1
+        else
+            bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
+            echo 'Successful setting tcp congestion control'
+            bbr_tcc=2
+        fi
 
-    if [[ $bbr_qdisc -eq '1' && $bbr_tcc -eq '1' ]]; then
-        echo 'TCP-BBR has already setup !'
-    elif [[ $bbr_qdisc -eq '2' || $bbr_tcc -eq '2' ]]; then
-        echo '######################'
-        echo 'TCP-BBR'
-        echo '######################'
-        sysctl -p
-        sysctl net.ipv4.tcp_available_congestion_control
-    else
-        echo 'Failed to set up TCP-BBR'
-    fi
+        if [[ $bbr_qdisc -eq '1' && $bbr_tcc -eq '1' ]]; then
+            echo 'TCP-BBR has already setup !'
+        elif [[ $bbr_qdisc -eq '2' || $bbr_tcc -eq '2' ]]; then
+            echo '######################'
+            echo 'TCP-BBR'
+            echo '######################'
+            sysctl -p
+            sysctl net.ipv4.tcp_available_congestion_control
+        else
+            echo 'Failed to set up TCP-BBR'
+        fi
     ;;
-  12)
-    apt-get update
-    apt-get dist-upgrade
-    apt-get clean
+    12)
+        apt-get update
+        apt-get dist-upgrade
+        apt-get clean
     ;;
-  13)
-    rpi-update
-    reboot
+    13)
+        rpi-update
+        reboot
     ;;
-  14)
-    echo 'Exited'
+    14)
+        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head
     ;;
-  *)
-    echo 'Tools not supported'
+    15)
+        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
+    ;;
+    16)
+        echo 'Exited'
+    ;;
+    *)
+        echo 'Tools not supported'
     ;;
 esac
 
