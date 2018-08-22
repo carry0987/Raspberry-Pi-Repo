8acde2770ddcc470e6350cff5bc96720563fcccb
carry0987 <carry0987@users.noreply.github.com>
2018-08-22 19:28:07 +0800

    Update Set-LC

--- Setup/Set-LC.sh
+++ Setup/Set-LC.sh
@@ -2,8 +2,19 @@
 
 set -e
 
-echo 'LANGUAGE=en_US.UTF-8' >> /etc/default/locale
-echo 'LC_ALL=en_US.UTF-8' >> /etc/default/locale
-echo 'LC_TYPE=en_US.UTF-8' >> /etc/default/locale
+rm /etc/default/locale
+touch /etc/default/locale
+chmod 644 /etc/default/locale
+
+read -p 'Please type your Language>' language
+
+echo 'LANG='$language >> /etc/default/locale
+echo 'LANGUAGE='$language >> /etc/default/locale
+echo 'LC_ALL='$language >> /etc/default/locale
+echo 'LC_TYPE='$language >> /etc/default/locale
+
+echo '#####################'
+cat /etc/default/locale
+echo '#####################'
 
 sudo reboot
