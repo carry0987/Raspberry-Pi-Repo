e0234c6c3b0b70685506a1b8f7f3700b547eaebb
carry0987 <carry0987@users.noreply.github.com>
2018-08-24 17:01:50 +0800

    Add sender in report-ip

--- Setup/Install-Main-Pkg.sh
+++ Setup/Install-Main-Pkg.sh
@@ -39,46 +39,62 @@ apt-get update
 apt-get dist-upgrade
 apt-get install zip vsftpd unzip wget vim screen smartmontools
 apt-get clean
-wget -P /usr/local/bin https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Auto-WiFi-Reconnect/wifi-reconnect.sh
-chmod +x /usr/local/bin/wifi-reconnect.sh
-echo '* * * * * root /usr/local/bin/wifi-reconnect.sh' >> /etc/crontab
 
-read -p 'Do you want to install IP Auto Reporter? [Y/N]' var
+#Set up vsftpd
+sed -i 's/ssl_enable=NO/ssl_enable=YES/g' /etc/vsftpd.conf
+sed -i 's/#local_umask=022/local_umask=022/g' /etc/vsftpd.conf
+sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
+sed -i 's/#utf8_filesystem=YES/utf8_filesystem=YES/g' /etc/vsftpd.conf
+systemctl start vsftpd
+systemctl enable vsftpd
+
+echo '1) Set WiFi Reconnect'
+echo '2) Set Auto Report IP'
+echo '3) Exit'
+read -p 'Please choose what you want to do>' var
 
 case $var in
-  [Yy])
-    echo 'Installing IP Auto Reporter...'
-    wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Auto-Report-IP/report-ip.py
-    read -p 'Please enter this device name>' name
-    echo 'Name: ' $name
-    sed -i 's/sender = "RPi"/sender = "'${name}'"/g' /home/pi/report-ip.py
-    read -p 'Please enter sender email address> ' email
-    echo 'Sender email address is '$email
-    sed -i 's/username = "Sender@gmail.com"/username = "'${email}'"/g' /home/pi/report-ip.py
-    echo "If you don't have the App Password, please go here to get the password: \nhttps://security.google.com/settings/security/apppasswords"
-    read -p 'Please enter your email password> ' password
-    sed -i 's/password = "Sender Password"/password = "'${password}'"/g' /home/pi/report-ip.py
-    read -p 'Please enter receiver email address> ' receiver
-    echo 'Receiver Email is '$receiver
-    sed -i 's/receiver = \["Receiver@gmail.com"\]/receiver = \["'$receiver'"\]/g' /home/pi/report-ip.py
-    chmod +x report-ip.py
-    mv report-ip.py /usr/local/bin
-    wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Auto-Report-IP/report-ip.service
-    mv report-ip.service /etc/systemd/system
-    systemctl enable report-ip
-    systemctl start report-ip
-    systemctl status report-ip
-    echo '* * * * * root /usr/bin/python3.5 /usr/local/bin/report-ip.py' >> /etc/crontab
+    1)
+        wget -P /usr/local/bin https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Auto-WiFi-Reconnect/wifi-reconnect.sh
+        chmod +x /usr/local/bin/wifi-reconnect.sh
+        echo '* * * * * root /usr/local/bin/wifi-reconnect.sh' >> /etc/crontab
     ;;
-  [Nn])
-    echo 'Setting up...'
+    2)
+        echo 'Installing IP Auto Reporter...'
+        wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Auto-Report-IP/report-ip.py
+        read -p 'Please enter this device name>' name
+        echo 'Name: ' $name
+        sed -i 's/sender = "RPi"/sender = "'${name}'"/g' /home/pi/report-ip.py
+        read -p 'Please enter sender email address> ' email
+        echo 'Sender email address is '$email
+        sed -i 's/username = "Sender@gmail.com"/username = "'${email}'"/g' /home/pi/report-ip.py
+        echo "If you don't have the App Password, please go here to get the password:"
+        echo 'https://security.google.com/settings/security/apppasswords'
+        read -p 'Please enter your email password> ' password
+        sed -i 's/password = "Sender Password"/password = "'${password}'"/g' /home/pi/report-ip.py
+        read -p 'Please enter receiver email address> ' receiver
+        echo 'Receiver Email is '$receiver
+        sed -i 's/receiver = \["Receiver@gmail.com"\]/receiver = \["'$receiver'"\]/g' /home/pi/report-ip.py
+        chmod +x report-ip.py
+        mv report-ip.py /usr/local/bin
+        wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Auto-Report-IP/report-ip.service
+        mv report-ip.service /etc/systemd/system
+        systemctl enable report-ip
+        systemctl start report-ip
+        systemctl status report-ip
+        echo '* * * * * root /usr/bin/python3.5 /usr/local/bin/report-ip.py' >> /etc/crontab
     ;;
-  *)
-    echo 'You can only choose Yes or No'
-    exit 2
+    3)
+        echo 'Exited'
+        exit 0
+    ;;
+    *)
+        echo 'You can only choose Yes or No'
+        exit 0
     ;;
 esac
 
+echo 'Setting up...'
 service cron restart
 echo 'Wait 5 seconds to reboot...'
 sleep 5
