bece4138b4080889e09982abc06c2512e236facc
carry0987 <carry0987@users.noreply.github.com>
2018-09-27 23:45:09 +0800

    Fix extension

--- Youtube-Upload/youtube-upload.sh
+++ Youtube-Upload/youtube-upload.sh
@@ -33,23 +33,44 @@ else
     file_path=${upload_list%/}
 fi
 
+#Get file type
+read -p 'Please enter the file extension of videos that you want to upload>' file_type
+
 #Check user
 check_user=$USER
 if [ $check_user == 'root' ]; then
     read -p 'The current user is Root now, please enter your youtube-upload user or leave blank if you want to run youtube-upload under Root>' select_user
-        for video_file in $file_path/*
-        do
-            filename=$(basename -- "$video_file")
-            video_name="${filename%.*}"
-            su $select_user -c "youtube-upload --client-secrets='/home/pi/.config/youtube-upload/youtube-upload-client-secrets.json' --credentials-file='/home/pi/.config/youtube-upload/youtube-upload-credentials.json' --privacy=private --title='$video_name' '$video_file'"
-        done
+    for video_file in $file_path/*.$file_type
+    do
+        filename=$(basename -- "$video_file")
+        video_name="${filename%.*}"
+        su $select_user -c "youtube-upload --client-secrets='/home/pi/.config/youtube-upload/youtube-upload-client-secrets.json' --credentials-file='/home/pi/.config/youtube-upload/youtube-upload-credentials.json' --privacy=private --title='$video_name' '$video_file'"
+    done
+    echo 'Upload Finished !'
+    read -p 'Do you want to remove all videos from local? [Y/N] ' remove_dir
+    if [[ $remove_dir =~ ^([Yy])+$ ]]; then
+      rm -vRf $file_path/*.$file_type
+    elif [[ $remove_dir =~ ^([Nn])+$ ]]; then
+      exit 0
+    else
+      echo 'You can only choose yes or no'
+    fi
 else
-        for video_file in $file_path/*
-        do
-            filename=$(basename -- "$video_file")
-            video_name="${filename%.*}"
-            su $USER -c "youtube-upload --client-secrets='/home/pi/.config/youtube-upload/youtube-upload-client-secrets.json' --credentials-file='/home/pi/.config/youtube-upload/youtube-upload-credentials.json' --privacy=private --title='$video_name' '$video_file'"
-        done
+    for video_file in $file_path/*.$file_type
+    do
+        filename=$(basename -- "$video_file")
+        video_name="${filename%.*}"
+        su $USER -c "youtube-upload --client-secrets='/home/pi/.config/youtube-upload/youtube-upload-client-secrets.json' --credentials-file='/home/pi/.config/youtube-upload/youtube-upload-credentials.json' --privacy=private --title='$video_name' '$video_file'"
+    done
+    echo 'Upload Finished !'
+    read -p 'Do you want to remove all videos from local? [Y/N] ' remove_dir
+    if [[ $remove_dir =~ ^([Yy])+$ ]]; then
+      rm -vRf $file_path/*.$file_type
+    elif [[ $remove_dir =~ ^([Nn])+$ ]]; then
+      exit 0
+    else
+      echo 'You can only choose yes or no'
+    fi
 fi
 
 exit 0
