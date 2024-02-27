0abe20a7d8febb2a872c14b9838eace7ce841093
carry0987 <carry0987@users.noreply.github.com>
2024-02-27 23:49:53 +0800

    Update wifi-reconnect.sh

--- Auto-WiFi-Reconnect/wifi-reconnect.sh
+++ Auto-WiFi-Reconnect/wifi-reconnect.sh
@@ -1,13 +1,20 @@
-#!/bin/bash 
- 
-SSID=$(/sbin/iwgetid --raw) 
+#!/bin/bash
 
-if [ -z "$SSID" ] 
-then 
+SSID=$(/sbin/iwgetid --raw)
+
+if [ -z "$SSID" ]; then
     echo "`date -Is` WiFi interface is down, trying to reconnect" >> /home/pi/wifi-log.txt
-    sudo ifconfig wlan0 down
-    sleep 10
-    sudo ifconfig wlan0 up 
-fi 
+    if command -v /sbin/ip &> /dev/null; then
+        /sbin/ip link set wlan0 down
+        sleep 10
+        /sbin/ip link set wlan0 up
+    elif command -v sudo ifconfig &> /dev/null; then
+        sudo ifconfig wlan0 down
+        sleep 10
+        sudo ifconfig wlan0 up
+    else
+        echo "`date -Is` Failed to reconnect: neither /sbin/ip nor ifconfig commands are available" >> /home/pi/wifi-log.txt
+    fi
+fi
 
 echo 'WiFi check finished'
